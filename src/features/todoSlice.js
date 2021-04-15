2534ecf344cc25310cd4088852a3d901ef45e711
Inokcode <regilearn@gmail.com>
2021-04-15 13:18:15 +0530

    First commit

---
+++ src/features/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//const[todoList,setTodoList] = useState([])
+
+//useState = initialState
+//todoList = state(current)
+//setTodoList = saveTodo
+
+const initialState = {
+  todoList: [],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    saveTodo: (state, action) => {
+      state.todoList.push(action.payload);
+    },
+  },
+});
+
+export const { saveTodo } = todoSlice.actions;
+export default todoSlice.reducer;
